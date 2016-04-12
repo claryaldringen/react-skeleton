28609417e4d61d532adf21fb41f080e41fad0354
clary <clary@livesport.eu>
2016-04-12 14:02:05 +0200

    First commit

---
+++ src/components/List.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+var ListItem = require('./ListItem.jsx');
+
+var ingredients = [{id: 1, name: 'ham'}, {id: 2, name: 'cheese'}, {id: 3, name: 'potato'}];
+
+class List extends React.Component{
+
+	render() {
+		var listItems = ingredients.map(function(item){
+			return <ListItem key={item.id} ingredient={item.name} />;
+		});
+
+		return(<ul>{listItems}</ul>);
+	}
+}
+
+module.exports = List;
